d0357ede2b792b012db83bd405270ecf82d5a3aa
Hamad2023CSU <h.alyami@colostate.edu>
2024-12-13 05:22:50 -0700

    Added a timer functionality through a function for 20 mins

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
+function timerFeatureInPage() {
+    const button = document.getElementById('start');
 
+    button.addEventListener('click', function () {
+        this.disabled = true;
+        timer(20);
+    })
 
+    function timer(durationInMinutes) {
+        let duration = durationInMinutes * 60;
+        --duration;
+        const textDivDisplay = document.getElementById('clock');
+
+        const timerInterval = setInterval(function() {
+            let textInMinutes = Math.floor(duration / 60);
+            let textInSeconds = duration % 60;
+            let timeInText = `${textInMinutes < 10 ? '0' : ''}${textInMinutes}:${textInSeconds < 10 ? '0' : ''}${textInSeconds}`;
+
+            textDivDisplay.textContent = timeInText;
+
+            if (duration > 0) {
+                duration--;
+            }
+            else {
+                clearInterval(timerInterval);
+                textDivDisplay.textContent = '00:00';
+                alert("Time is up!");
+                button.disabled = false;
+            }
+        }, 1000);
+    }
+}
+
+timerFeatureInPage();
