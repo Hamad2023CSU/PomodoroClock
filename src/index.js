b47e297beb877b4ba59c379de9dfcaafbbc985f4
Hamad2023CSU <h.alyami@colostate.edu>
2024-12-29 00:40:19 -0700

    Added pause and resume function to 20 min timer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 function timerFeatureInPage() {
-    const button = document.getElementById('start');
+    const startButton = document.getElementById('start');
+    const pauseButton = document.getElementById('pause');
+    const resumeButton = document.getElementById('resume');
 
-    button.addEventListener('click', function () {
-        this.disabled = true;
+    let pauseFlag = false;
+
+    startButton.addEventListener('click', function () {
+        startButton.style.display = 'none';
+        pauseButton.style.display = "inline-block";
         timer(20);
+    })
+
+    pauseButton.addEventListener('click', function () {
+        pauseFlag = true;
+        pauseButton.style.display = "none";
+        resumeButton.style.display = "inline-block";
+    })
+
+    resumeButton.addEventListener('click', function () {
+        resumeButton.style.display = "none";
+        pauseButton.style.display = "inline-block";
+        pauseFlag = false;
     })
 
     function timer(durationInMinutes) {
@@ -12,6 +29,10 @@ function timerFeatureInPage() {
         const textDivDisplay = document.getElementById('clock');
 
         const timerInterval = setInterval(function() {
+            if (pauseFlag) {
+                return;
+            }
+
             let textInMinutes = Math.floor(duration / 60);
             let textInSeconds = duration % 60;
             let timeInText = `${textInMinutes < 10 ? '0' : ''}${textInMinutes}:${textInSeconds < 10 ? '0' : ''}${textInSeconds}`;
